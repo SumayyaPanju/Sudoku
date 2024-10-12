d4c190ced9de2862caa07a18b57aab4a0545cdc9
Sumayya Mussa <panjusumayya@gmail.com>
2024-10-12 03:42:40 +0000

    Functions to activate dark theme

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,3 +7,19 @@ const buttonsText = document.getElementsByClassName("buttons-text");
 const labels = document.getElementsByTagName("label");
 const lightModeOption = document.getElementById("light-mode");
 const darkModeOption = document.getElementById("dark-mode");
+
+//Functions to activate Dark Theme
+function darkThemeActivation() {
+    //Change background background colour
+    myBody.style.backgroundColor = "#1c1414";
+    //Change container background colour
+    homeContainer.style.backgroundColor = "#1c1414";
+    //Loop that allows all classes of 'buttons-text' to be selected and styled
+    for (let item of buttonsText) {
+      item.style.backgroundColor = "#ECDFCC";
+      item.style.color = "#1c1414";
+    }
+    //Loop that allows all tags of 'label' to be selected and styled
+    for (let item of labels) {
+      item.style.color = "#ECDFCC";
+    }}
