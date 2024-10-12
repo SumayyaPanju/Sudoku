f38f3e64def717709039fec5c3b63de683c60382
Sumayya Mussa <panjusumayya@gmail.com>
2024-10-12 03:41:41 +0000

    Variables defined

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------Changing themes*/
+
+//Variables defined
+const myBody = document.getElementsByTagName("body")[0];
+const homeContainer = document.getElementById("home-container");
+const buttonsText = document.getElementsByClassName("buttons-text");
+const labels = document.getElementsByTagName("label");
+const lightModeOption = document.getElementById("light-mode");
+const darkModeOption = document.getElementById("dark-mode");
